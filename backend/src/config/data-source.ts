12fe3b716f668aba385830a94ef180fbba9a3764
Jeremy Nayet <71263473+Jeremy-Epitech@users.noreply.github.com>
2023-03-01 13:12:05 +0100

    Fix: backend connec to db docker

--- backend/src/config/data-source.ts
+++ backend/src/config/data-source.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: 'mysql',
-  host: process.env.HOST,
+  host: process.env.MYSQL_HOST,
   port: parseInt(process.env.MYSQL_PORT ?? '3306'),
   username: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
